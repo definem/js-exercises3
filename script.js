ae7950e8ac969f7e8a8795aafed018e1ae041bc8
define) <120991965+definem@users.noreply.github.com>
2023-06-08 02:53:00 +0500

    for in object

--- script.js
+++ script.js
@@ -7,3 +7,13 @@
 //   { name: "Nasıl dayandın Ya Resülüllah", author: "Mehmet Yıldız", price: "70.000",},
 //   { name: "İslamiın kızına", author: "Hoca Mehmet", price: "86.000" },
 // ];
+
+
+// for (let i = 0; i < books.length; i++) {
+//   if (books[i].price <= 70) {
+//     console.log(`İstediğiniz kitab ${books[i].name}`);
+//   }
+//   console.log(
+//     `İstediğiniz kitab'ın sahibi: ${books[i].author}, fıyatı:${books[i].price}.`
+//   );
+// }
