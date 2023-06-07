be7cd83483abf857fbe9b9cae939b58e9cc25ffe
define) <120991965+definem@users.noreply.github.com>
2023-06-08 02:52:23 +0500

    object

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // < ---- Bismillah ---- >
+
+// let books = [
+//   { name: "Love for God", author: "One Person", price: "98.000" },
+//   { name: "Çalıkuşu", author: "Reşat Nurı Güntekin", price: "50.000" },
+//   { name: "Hedefimiz Cennet", author: "Hanife", price: "48.000" },
+//   { name: "Nasıl dayandın Ya Resülüllah", author: "Mehmet Yıldız", price: "70.000",},
+//   { name: "İslamiın kızına", author: "Hoca Mehmet", price: "86.000" },
+// ];
