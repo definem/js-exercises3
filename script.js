440f30cdd0990cedafda9c0d4ca309ace9a9c045
define) <120991965+definem@users.noreply.github.com>
2023-06-08 02:54:33 +0500

    CodeWars exercise-1

--- script.js
+++ script.js
@@ -22,3 +22,15 @@
 // function sumTwoSmallestNumbers(numbers) {  
 //   var variable = numbers.sort(function(a, b){return a - b;});
 //   var result = 0;
+
+//   for(i = 0; i < variable.length; i++){
+//     if(i == 0){
+//       result+=variable[0];
+//     }
+//     if(i == 1){
+//       result += variable[1];
+//     }
+//   }
+//   return result;
+// //Code here
+// };
