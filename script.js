b387ec27153e257c965cd1a907f0f22071db35ab
define) <120991965+definem@users.noreply.github.com>
2023-06-08 02:55:45 +0500

    CodeWars exercise-2

--- script.js
+++ script.js
@@ -34,3 +34,16 @@
 //   return result;
 // //Code here
 // };
+
+// CodeWars exercise-2
+
+// < ---- In progress... ---- >
+
+let count = 0;
+let result = 0;
+
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15],
+
+for(i = 0; i < ){
+
+}
